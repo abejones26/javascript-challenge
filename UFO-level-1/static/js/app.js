957d7347890229b0a3737219266f3645924b09e8
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2020-04-01 22:47:42 -0500

    Final commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,17 +1,22 @@
-// from data.js
-// var tableData = data;
+// This is where my results will be appended
 var tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
 console.log(data);
 
+// This function append the filtereddata results into a table on the html
+// Looping through 'data
 data.forEach(function(sightings) {
     console.log(sightings);
+
+    // Using d3 to append one table row 'tr' for each sighting
     var row = tbody.append("tr");
 
+    // Using object.entries to console.log my value results
     Object.entries(sightings).forEach(function([key, value]) {
         console.log(key, value);
 
+        // Appending value results into the html table
         var cell = row.append("td");
         cell.text(value);
     });
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,24 +1,31 @@
-// from data.js
-// var tableData = data;
+// This is where my results will be appended
 var tbody = d3.select("tbody");
 
+// Data source
 var sightdate = data;
 
+// Calling the filter button from the html here
 var button = d3.select("#filter-btn");
 
 button.on("click", function() {
 
+    // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
 
+    // Get the value property of the input element
     var inputValue = inputElement.property("value");
 
+    // Printing both inputValue and sightdate into the console
     console.log(inputValue);
     console.log(sightdate);
 
+    // This is my filter parameter
     var filteredData = sightdate.filter(sightdate => sightdate.datetime === inputValue);
 
+    // Print the fileteredData results into the console
     console.log(filteredData);
 
+    // This function append the filtereddata results into a table on the html
     filteredData.forEach(function(sightings) {
         console.log(sightings);
         var row = tbody.append("tr");
